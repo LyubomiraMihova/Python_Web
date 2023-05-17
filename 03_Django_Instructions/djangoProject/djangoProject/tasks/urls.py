c5a2870663deef8e5e2b901c79837e57114f0394
LyubomiraMihova <lyubomiramihova@gmail.com>
2023-05-17 22:57:22 +0300

    Start Django Introduction

--- 03_Django_Instructions/djangoProject/djangoProject/tasks/urls.py
+++ 03_Django_Instructions/djangoProject/djangoProject/tasks/urls.py
@@ -1,7 +1,10 @@
 # urls for tasks app
 from django.urls import path
-from djangoProject.tasks.views import index
+from djangoProject.tasks.views import index, list_tasks_template
+# , list_tasks
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    # path('list/', list_tasks)
+    path('with-template/', list_tasks_template)
 ]
--- 03_Django_Instructions/djangoProject/djangoProject/tasks/views.py
+++ 03_Django_Instructions/djangoProject/djangoProject/tasks/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from djangoProject.tasks.models import Task
 
 # Create your views here.
 
 def index(request):
-    pass
+    return HttpResponse('YES!')
 
+# def list_tasks(request):
+#     all_tasks = Task.objects.all()
+#
+#     return HttpResponse(f'{t.id} {t.title} {t.priority}' for t in all_tasks)
+
+def list_tasks_template(request):
+    return render(request, 'tasks.html')
