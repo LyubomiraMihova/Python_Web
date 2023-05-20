bf2bfaf5b5f84ebe3dc2ac158cb3c6fec1e4c249
LyubomiraMihova <lyubomiramihova@gmail.com>
2023-05-20 23:30:20 +0300

    Added demos for urls and views

--- 04_URLs_and_Views/departments_app/departments_app/departments/urls.py
+++ 04_URLs_and_Views/departments_app/departments_app/departments/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from departments_app.departments.views import sample_view
+from departments_app.departments.views import sample_view, show_departments, redirect_to_first_department, \
+    show_not_found
 
 urlpatterns = (
     path('', sample_view),
-    path('<int:department_id>/', sample_view)
+    path('<int:department_id>/', sample_view),
+    path('dep', show_departments, name='show departments'),
+    path('redirect/', redirect_to_first_department, name='redirect demo'),
+    path('not-found/', show_not_found, name='not found'),
 )
--- 04_URLs_and_Views/departments_app/departments_app/departments/views.py
+++ 04_URLs_and_Views/departments_app/departments_app/departments/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from random import choice
+
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpRequest, HttpResponseNotFound
+
 
 # Create your views here.
 
 def sample_view(request, *args, **kwargs):
     body = f'path: {request.path}, args={args}, kwargs={kwargs}'
     return HttpResponse(body)
+
+
+def show_departments(request: HttpRequest, *args, **kwargs):
+    context = {
+        'method': request.method,
+        'order_by': request.GET.get('order_by', 'name'),
+    }
+
+    return render(request, 'departments/all.html', context)
+
+
+def redirect_to_first_department(request):
+    possible_order_by = ['name', 'age', 'id']
+    order_by = choice(possible_order_by)
+    # return redirect(f'/departments/?order_by={order_by}')
+    return redirect('show departments')
+
+
+def show_not_found(request):
+    return HttpResponseNotFound()
+
