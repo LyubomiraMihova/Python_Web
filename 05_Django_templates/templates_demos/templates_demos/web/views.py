07a66f0c6c9084383f235fcc5afa9828792c5f35
LyubomiraMihova <lyubomiramihova@gmail.com>
2023-05-23 22:12:06 +0300

    django project

---
+++ 05_Django_templates/templates_demos/templates_demos/web/views.py
@@ -0,0 +1,34 @@
+import random
+from datetime import datetime
+
+from django.shortcuts import render, redirect
+
+
+# Create your views here.
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def get_info(self):
+        return f'Name: {self.name}; Age: {self.age}'
+
+
+def index(request):
+    context = {
+        'title': 'SoftUni Homepage',
+        'value': random.random(),
+        'info': {'address': 'Sofia'},
+        'student': Student('Doncho', 19),
+        'student_info': Student('Doncho', 19).get_info(),
+        'now': datetime.now(),
+        'students': ['Pesho', 'Gosho', 'Maria', 'Genata'],
+        # 'students': [],
+        'values': list(range(20)),
+    }
+
+    return render(request, 'index.html', context)
+
+
+def redirect_to_home(request):
+    return redirect('index')
