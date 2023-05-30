b2d43af5f5714513b96807cd7424b3a5bed07470
LyubomiraMihova <lyubomiramihova@gmail.com>
2023-05-30 23:21:22 +0300

    django project

--- Workshop_Petstagram/petstagram_project/petstagram_project/urls.py
+++ Workshop_Petstagram/petstagram_project/petstagram_project/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('petstagram_project.common.urls')),
+    path('accounts/', include('petstagram_project.accounts.urls')),
+    path('pets/', include('petstagram_project.pets.urls')),
+    path('photos/', include('petstagram_project.accounts.urls')),
 ]
