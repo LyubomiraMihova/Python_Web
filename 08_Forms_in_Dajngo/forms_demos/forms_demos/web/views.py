520215d7d4aa36d2dff51ddecaf206ce2ff433b9
LyubomiraMihova <lyubomiramihova@gmail.com>
2023-06-11 23:55:06 +0300

    Django Models

--- 08_Forms_in_Dajngo/forms_demos/forms_demos/web/views.py
+++ 08_Forms_in_Dajngo/forms_demos/forms_demos/web/views.py
@@ -1,25 +1,27 @@
+from django import forms
 from django.shortcuts import render
 
-# Create your views here.
 
-from django import forms
-
+# from forms_demos.web.forms import NameForm
 
 class NameForm(forms.Form):
     your_name = forms.CharField(label='Your name', max_length=100)
 
 
+# Create your views here.
+
+
 def index(request):
-    if request.method == 'POST':
-        print(request.POST)
+    if request.method == 'GET':
+        form = NameForm()
+    else:
         form = NameForm(request.POST)
         form.is_valid()
         name = form.cleaned_data['your_name']
-    else:
-        form = NameForm()
-        name = None
 
     context = {
-        'form': form
+        'form': form,
+        'name': name,
     }
+
     return render(request, 'web/index.html', context)
