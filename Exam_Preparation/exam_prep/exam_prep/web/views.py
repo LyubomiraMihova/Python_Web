41af6d9a83c54765279875c7a38cd0bb5cecf8ec
LyubomiraMihova <lyubomiramihova@gmail.com>
2023-07-01 22:14:48 +0300

    Exam preparation

---
+++ Exam_Preparation/exam_prep/exam_prep/web/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+# · http: // localhost: 8000 / - homepage
+# · http: // localhost: 8000 / album / add / - add album page
+# · http: // localhost: 8000 / album / details / < id > / - album details page
+# · http: // localhost: 8000 / album / edit / < id > / - edit album page
+# · http: // localhost: 8000 / album / delete / < id > / - delete album page
+# · http: // localhost: 8000 / profile / details / - profile details page
+# · http: // localhost: 8000 / profile / delete / - delete profile page
+
+def index(request):
+    pass
+
+
+def add_album(request):
+    pass
+
+
+def details_album(request, pk):
+    pass
+
+
+def edit_album(request, pk):
+    pass
+
+
+def delete_album(request, pk):
+    pass
+
+
+def details_profile(request):
+    pass
+
+def delete_profile(request):
+    pass
+
+
+
