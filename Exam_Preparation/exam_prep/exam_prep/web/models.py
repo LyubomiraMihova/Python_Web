baa59fc0ea7d78976e2adedeaefd751a7c679abf
LyubomiraMihova <lyubomiramihova@gmail.com>
2023-07-04 00:39:54 +0300

    Project

--- Exam_Preparation/exam_prep/exam_prep/web/models.py
+++ Exam_Preparation/exam_prep/exam_prep/web/models.py
@@ -1,3 +1,101 @@
+from django.core import validators
+from django.core.exceptions import ValidationError
 from django.db import models
 
-# Create your models here.
+
+def validate_only_alphanumeric(value):
+    for ch in value:
+        if not ch.isalnum() and '-' not in value:
+            raise ValidationError("Ensure this value contains only letters, numbers, and underscore.")
+
+
+class Profile(models.Model):
+    MIN_USERNAME_LENGTH = 2
+    MAX_USERNAME_LENGTH = 15
+    username = models.CharField(
+        max_length=MAX_USERNAME_LENGTH,
+        null=False,
+        blank=False,
+        validators=(
+            validators.MinLengthValidator(MIN_USERNAME_LENGTH),
+            validate_only_alphanumeric,
+        )
+    )
+    email = models.EmailField(
+        null=False,
+        blank=False,
+    )
+    age = models.PositiveIntegerField(
+        null=True,
+        blank=True,
+    )
+
+
+# Album#
+# o Price
+#
+# § Float field, required.
+#
+# § The number of decimal places of the price should not be specified in the database.
+#
+# § The price cannot be below 0.0.
+#
+# Note: the validations will be examined only by the user side, not the admin side
+
+class Album(models.Model):
+    MAX_LEN_NAME = 30
+    MAX_LEN_ARTIST = 30
+    MAX_LEN_GENRE = 30
+
+    POP_MUSIC = "Pop Music"
+    JAZZ_MUSIC = "Jazz Music"
+    RNB_MUSIC = "R&B Music"
+    ROCK_MUSIC = "Rock Music"
+    COUNTRY_MUSIC = "Country Music"
+    DANCE_MUSIC = "Dance Music"
+    HIP_HOP_MUSIC = "Hip Hop Music"
+    OTHER_MUSIC = "Other"
+
+    GENRE_CHOICES = (
+        (POP_MUSIC, POP_MUSIC),
+        (JAZZ_MUSIC, JAZZ_MUSIC),
+        (RNB_MUSIC, RNB_MUSIC),
+        (ROCK_MUSIC, ROCK_MUSIC),
+        (COUNTRY_MUSIC, COUNTRY_MUSIC),
+        (DANCE_MUSIC, DANCE_MUSIC),
+        (HIP_HOP_MUSIC, HIP_HOP_MUSIC),
+        (OTHER_MUSIC, OTHER_MUSIC),
+    )
+
+    album_name = models.CharField(
+        max_length=MAX_LEN_NAME,
+        unique=True,
+        null=False,
+        blank=False,
+    )
+    artist = models.CharField(
+        max_length=MAX_LEN_ARTIST,
+        null=False,
+        blank=False,
+    )
+    genre = models.CharField(
+        max_length=MAX_LEN_GENRE,
+        choices=GENRE_CHOICES,
+        null=False,
+        blank=False,
+    )
+    description = models.TextField(
+        null=True,
+        blank=True,
+    )
+    image_url = models.URLField(
+        null=False,
+        blank=False,
+    )
+    price = models.FloatField(
+        null=False,
+        blank=False,
+        validators=(
+            validators.MinValueValidator(0.0),
+        )
+    )
